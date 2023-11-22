e21eb62b0d94d5fc696cf8b0259473d5ec67ef31
Tandra Malm <tandra_malm@hotmail.com>
2023-11-22 14:13:19 -0700

    update file on github

--- play.js
+++ play.js
@@ -1,9 +1,9 @@
 const net = require("net");
 
 // establishes a connection w/ the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
-    host: "165.227.47.243", 
+    host: "165.227.47.243",
     port: 50541
   });
 
@@ -11,7 +11,7 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("connect", (data) => {
-    console.log('you ded cuz you idled', data);
+    console.log("You ded cuz you idled", data);
   });
 
   return conn;
