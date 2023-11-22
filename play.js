edb093d4d8eeacaf8729a6a97adfd299f089243e
Tandra Malm <tandra_malm@hotmail.com>
2023-11-22 16:32:03 -0700

    add file and add changes

--- play.js
+++ play.js
@@ -1,24 +1,8 @@
 const connect = require("./client");
+const setupInput = require("./input");
 
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === 'x') {
-    process.exit();
-  }
-  console.log('key', key);
-};
-
+console.log("Connecting ...");
 connect();
+
 setupInput();
 
