b7a735086a43b6ffd32eebd4d751f66f9ab254a8
Tandra Malm <tandra_malm@hotmail.com>
2023-11-22 16:07:38 -0700

    update files on github

--- client.js
+++ client.js
@@ -2,36 +2,33 @@ const net = require("net");
 
 // establishes a connection w/ the game server
 const connect = function() {
+  // create TCP connection
+  console.log("Connecting ...");
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541
   });
 
-  module.exports = connect;
+ 
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  
   conn.on('connect', function() {
     console.log('You are connected to the game server');
+    // send name to game to send to server
+    const userName = "tpm";
+    const displayMsg = `Name: ${userName}`;
+    conn.write(displayMsg);
   });
 
-  // send name to game
-  const userName = "tpm";
-  const displayMsg = `Name: ${userName}`;
-  conn.write(displayMsg);
-
-  conn.on('data', function(message){
+  conn.on('data', function(message) {
     console.log(`${message}`);
   });
 
-  process.stdin.on('name', function(message){
-    const addMessage = `${process.argv[2]} > ${message}`;
-    conn.write(addMessage);
-  })
-  
+
   return conn;
 };
 
-console.log("Connecting ...");
-connect();
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,5 +1,24 @@
 const connect = require("./client");
 
-const net = require("net");
 
-const conn = net.createServer();
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === 'x') {
+    process.exit();
+  }
+  console.log('key', key);
+};
+
+connect();
+setupInput();
+
