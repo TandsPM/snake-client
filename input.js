6e807414deb961e85078f2000b72a2d005a2ea65
Tandra Malm <tandra_malm@hotmail.com>
2023-11-22 17:00:56 -0700

    add changes

--- input.js
+++ input.js
@@ -1,4 +1,11 @@
-const setupInput = function () {
+//const connect = require("./client");
+
+
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -14,6 +21,20 @@ const handleUserInput = function (key) {
     process.exit();
   }
   console.log('key', key);
+
+
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
 };
 
 module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,8 +1,10 @@
 const connect = require("./client");
 const setupInput = require("./input");
 
-console.log("Connecting ...");
-connect();
 
-setupInput();
+
+console.log("Connecting ...");
+const connection = connect();
+
+setupInput(connection);
 
